41192df5f34e4b516351249581d08f6803ea9181
Akku0039 <117562663+Akku0039@users.noreply.github.com>
2023-12-08 13:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species=species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log('Animal make Generic sound');
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark() {
+        console.log("woof");
+    }
 
-class Cat extends Animal {}
+    // Override the makeSound method for Dog
+    makeSound() {
+        console.log("Woof");
+    }
+}
+
+class Cat extends Animal {
+	purr() {
+        console.log("purr");
+    }
+
+    // Override the makeSound method for Cat
+    makeSound() {
+        console.log("Meow");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
